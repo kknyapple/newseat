6eb142a20e94dce48ffd8df23104684e819ec2db
kknyapple <kknyapple@gmail.com>
2023-02-26 06:21:17 +0900

    feat: 링크 이동 시 스크롤를 맨 위로 올려주는 기능

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import ScrollToTop from "./ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </RecoilRoot>
